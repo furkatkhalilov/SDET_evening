1b102bff595494001d7cbe7e3ec95ac08575c221
mhmmtkkl <makoklu32@gmail.com>
2020-06-19 19:09:42 -0400

    Day3

--- src/study/techno/day3/_01_Intro.java
+++ src/study/techno/day3/_01_Intro.java
@@ -1,4 +1,18 @@
 package study.techno.day3;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class _01_Intro {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Selenium dependency\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.practiceselenium.com/");
+
+        driver.quit();
+    }
 }
