530f453e8a82f7e3191a5f7f254ab0b65948cd24
Dauke <daulet2030@gmail.com>
2020-06-19 07:49:53 +0600

    selenium

--- src/study/techno/day0/Main.java
+++ src/study/techno/day0/Main.java
@@ -1,8 +1,10 @@
 package study.techno.day0;
 
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://google.com" );
 
-    public static void main(String[] args) {
-	// write your code here
     }
 }
