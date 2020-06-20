c441e907a93e0fcdea14d75f160b2ac1de1c2d53
mhmmtkkl <makoklu32@gmail.com>
2020-06-19 20:00:11 -0400

    Day3

--- src/study/techno/day3/_02_FindingElements.java
+++ src/study/techno/day3/_02_FindingElements.java
@@ -1,6 +1,8 @@
 package study.techno.day3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class _02_FindingElements {
@@ -14,7 +16,24 @@ public class _02_FindingElements {
 //        Navigating to the website
         driver.get("http://www.practiceselenium.com/");
 
-        
+//        can we open browser full screen?
+        driver.manage().window().maximize();
+
+        WebElement element = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000450914886"));
+
+        System.out.println(element.getText());
+
+        WebElement element2 = driver.findElement(By.id("wsb-element-00000000-0000-0000-0000-000450914887"));
+        System.out.println(element2.getText());
+
+        /*
+        If you see the NoSuchElementException
+            First check the locator
+                If the locator correct then put some waits before the line which is failing.
+         */
+        WebElement elementWrongID = driver.findElement(By.id("this is wrong id"));
+        System.out.println(elementWrongID.getText());
+
 
     }
 }
