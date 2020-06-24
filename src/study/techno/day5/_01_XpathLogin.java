994ca455a69832f8e3d5117078d6200e32d460f3
mhmmtkkl <makoklu32@gmail.com>
2020-06-23 20:37:59 -0400

    Day5

--- src/study/techno/day5/_01_XpathLogin.java
+++ src/study/techno/day5/_01_XpathLogin.java
@@ -34,5 +34,32 @@ public class _01_XpathLogin {
         WebElement bikeLightElement = driver.findElement(By.xpath("//div[text()='Sauce Labs Bike Light']"));
         bikeLightElement.click();
 
+        /*
+            Parent to child
+                //div[@class='inventory_details_desc_container']/button <-- single slash is looking for direct child
+                //div[@class='inventory_details_container']//div  <-- double slash is looking for the all child
+         */
+
+        WebElement addToCart = driver.findElement(By.xpath("//button[text()='ADD TO CART']"));
+        addToCart.click();
+
+        WebElement buttonBack = driver.findElement(By.xpath("//button[text()='<- Back']"));
+        buttonBack.click();
+
+//        //div[text()='Sauce Labs Bolt T-Shirt']
+//        (//div[@class='inventory_item_name'])[3]
+
+        WebElement boltTshirtText = driver.findElement(By.xpath("//div[text()='Sauce Labs Bolt T-Shirt']"));
+        boltTshirtText.click();
+
+        addToCart = driver.findElement(By.xpath("//button[text()='ADD TO CART']"));
+        addToCart.click();
+
+        WebElement cartIcon = driver.findElement(By.xpath("//div[@id='shopping_cart_container']"));
+        cartIcon.click();
+
+
+
+
     }
 }
