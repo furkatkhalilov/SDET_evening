a9dee884ad1d77cbadde1ef34c9ef01d3f7a0f98
mhmmtkkl <makoklu32@gmail.com>
2020-06-26 21:19:25 -0400

    Day7

--- src/study/techno/Day7/_03_SelectClassPart2.java
+++ src/study/techno/Day7/_03_SelectClassPart2.java
@@ -1,10 +1,42 @@
 package study.techno.Day7;
 
-public class _03_SelectClassPart2 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import study.techno.Utils.BaseDriver;
+
+public class _03_SelectClassPart2 extends BaseDriver {
 
     /*
     Navigate to https://www.amazon.com/
 
     Choose amazon fresh
      */
+
+    public static void main(String[] args) {
+
+        driver.get("https://www.amazon.com/");
+
+        WebElement menuDropdown = driver.findElement(By.id("searchDropdownBox"));
+
+        Select s1 = new Select(menuDropdown);
+
+//        s1.selectByVisibleText("Amazon Fresh");
+        s1.selectByIndex(4);
+
+        WebElement searchButton = driver.findElement(By.xpath("//div[@class='nav-search-submit nav-sprite']"));
+        searchButton.click();
+
+        /*
+        WebElement searchButton = driver.findElement(By.id("nav-search-submit-text"));
+        searchButton.click(); is not working for click on the search button in amazon
+
+        ElementClickInterceptedException
+            There is one element front of your webelement
+
+         */
+
+    }
+
+
 }
---
+++ src/study/techno/Day7/_04_SelectClassFacebook.java
@@ -0,0 +1,26 @@
+package study.techno.Day7;
+
+import study.techno.Utils.BaseDriver;
+
+public class _04_SelectClassFacebook extends BaseDriver {
+
+    public static void main(String[] args) {
+
+        /*
+
+        Navigate to https://www.facebook.com/
+
+        Enter first name lastname mobile password
+
+        Choose November in month
+
+        Choose 25 in days
+
+        Choose 2000 in years
+
+        Click on
+         */
+
+
+    }
+}
