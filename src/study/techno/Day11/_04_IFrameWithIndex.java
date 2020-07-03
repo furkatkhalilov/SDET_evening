081820a8841d3cecd2560545e5fce10aef4f942c
mhmmtkkl <makoklu32@gmail.com>
2020-07-03 19:44:37 -0400

    Day11

--- src/study/techno/Day11/_04_IFrameWithIndex.java
+++ src/study/techno/Day11/_04_IFrameWithIndex.java
@@ -1,6 +1,10 @@
 package study.techno.Day11;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import study.techno.Utils.BaseDriver;
+
+import java.util.List;
 
 public class _04_IFrameWithIndex extends BaseDriver {
 
@@ -8,7 +12,23 @@ public class _04_IFrameWithIndex extends BaseDriver {
 
         driver.get("https://chercher.tech/practice/frames");
 
+        List<WebElement> IFrameList = driver.findElements(By.tagName("iframe"));
+        System.out.println(IFrameList.size());
 
+        for(int i = 0 ; i<IFrameList.size() ; i++){
+
+            driver.switchTo().frame(i);
+            int frameInTheSize = driver.findElements(By.tagName("iframe")).size();
+
+            System.out.println("Frame in the frame count = " + frameInTheSize);
+
+            /*
+                Without this line Selenium is not able to see the second frame
+                    Because when we switch to first one It is just focusing to the first frame
+                    and not able to see the rest of the page.
+             */
+            driver.switchTo().parentFrame();
+        }
     }
 
 }
---
+++ src/study/techno/Day11/_05_ExerciseIFrame.java
@@ -0,0 +1,21 @@
+package study.techno.Day11;
+
+import study.techno.Utils.BaseDriver;
+
+public class _05_ExerciseIFrame extends BaseDriver {
+
+    public static void main(String[] args) {
+
+        /*
+            Navigate to https://www.123formbuilder.com/free-form-templates/
+
+            Click on the Online Event Registration Form
+
+            Enter the first name
+
+            Then click on Continue with template
+ 
+         */
+
+    }
+}
