85101994605d77e20cef4b760b1fdfcbe3101783
mhmmtkkl <makoklu32@gmail.com>
2020-07-07 20:53:31 -0400

    Day13

--- src/study/techno/Day13/_05_ActionExercise.java
+++ src/study/techno/Day13/_05_ActionExercise.java
@@ -1,10 +1,18 @@
 package study.techno.Day13;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import study.techno.Utils.BaseDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class _05_ActionExercise extends BaseDriver {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         /*
             navigate to https://www.yogaalliance.org/--
@@ -15,7 +23,37 @@ public class _05_ActionExercise extends BaseDriver {
 
             Verify URL is containing the Teachers text.
 
-            until 8.28
+
          */
+
+        driver.get("https://www.yogaalliance.org/");
+
+        WebElement BecomeAMember = driver.findElement(By.linkText("Become a Member"));
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(BecomeAMember).perform();
+
+        //        (//a[text()='Teachers'])[2] other option for the teachers element
+        WebElement teachers = driver.findElement(By.xpath("//li[@class='category child-3']/a[text()='Teachers']"));
+        teachers.click();
+
+        WebDriverWait wait = new WebDriverWait(driver,10);
+//        wait.until(ExpectedConditions.invisibilityOf(teachers)); waiting until element invisible
+        wait.until(ExpectedConditions.titleContains("Teachers | Yoga Alliance"));
+
+        String URL = driver.getCurrentUrl();
+
+        System.out.println(URL);
+
+        Assert.assertTrue(URL.contains("Teachers"));
+
+        /*
+            Why Implicit wait is not working?
+               driver.manage().timeouts().implicitlyWait(10 , TimeUnit.SECONDS);
+
+               Implicit wait is waiting if there is a noSuchElementException.
+
+         */
+
     }
 }
